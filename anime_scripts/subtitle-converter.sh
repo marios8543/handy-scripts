e5acec35134b58c17b5b8cd9a32b4f6bca1b6d4f
marios <marios8543@gmail.com>
2020-04-03 14:33:37 +0300

    Update subtitle-converter.sh

--- anime_scripts/subtitle-converter.sh
+++ anime_scripts/subtitle-converter.sh
@@ -4,6 +4,7 @@ function convert() {
 	res=$(ffprobe -v error -show_entries stream=codec_type,codec_name -of compact "$1" | grep -s "subtitle");
 	if [[ "$res" == *"ass"* ]]; then
 		echo "Converting $1";
+		ext="${1##*.}";
 		ffmpeg -n -i "$1" -c:s srt "${i%.$ext}.srt" > /dev/null 2&> /dev/null;
 	else
 		echo "$1 not ASS. Skipping";
@@ -14,12 +15,10 @@ if [[ -d "$1" ]]; then
 	cd "$1";
 	shopt -s globstar
 	for i in **/*; do
-		ext="${i##*.}";
 		convert "$i";
 	done
 elif [[ -f "$1" ]]; then
 	cd $(dirname "$1");
 	name=$(basename "$1");
-	ext="${name##*.}";
 	convert "$name";
 fi
